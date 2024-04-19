8a682708b190bfc7a686b20c1b50cef7501f8d72
alehandrogusev <agusev@erpone.ru>
2024-04-19 16:09:53 +0300

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   env.d.ts 	new file:   index.html 	new file:   localhost-key.pem 	new file:   localhost.pem 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/SimpleForm.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.ts 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+server: {
+
+  https: {
+    key: './localhost-key.pem',
+    cert: './localhost.pem',
+  }
+},
+
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
