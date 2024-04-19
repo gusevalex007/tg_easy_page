42ade26fb69a4a119d55a9b41d7cc01ae273e332
alehandrogusev <agusev@erpone.ru>
2024-04-19 16:43:15 +0300

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-server: {
+// server: {
 
-  https: {
-    key: './localhost-key.pem',
-    cert: './localhost.pem',
-  }
-},
+//   https: {
+//     key: './localhost-key.pem',
+//     cert: './localhost.pem',
+//   }
+// },
 
   plugins: [
     vue(),
